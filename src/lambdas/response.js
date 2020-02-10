d79dd5ec45f4bbd0de7c59a8553e0a52564f705f
uillerroger <uillerrogermartins@gmail.com>
2020-02-10 17:08:25 -0300

    country

--- src/lambdas/response.js
+++ src/lambdas/response.js
@@ -7,21 +7,22 @@ const querystring = require('querystring')
 
 const response = async (event, context, callback) => {
     
-    const memory = querystring.parse(event.body).Memory
-  
-    console.log(event)
-    
-    console.log(event.body.Memory)
-    // let responseObject = {};
+    const memory = event.body.Memory
+
+    console.log(memory)   
+
+    let responseObject = {};
         
 
-    // let first_name = memory.twilio.collected_data.collect_clothes_order.answers.first_name.answer;
-    // let clothes_type = memory.twilio.collected_data.collect_clothes_order.answers.clothes_type.answer;
-    // let num_clothes = memory.twilio.collected_data.collect_clothes_order.answers.num_clothes.answer;
+    let first_name = memory.twilio.collected_data.collect_clothes_order.answers.first_name.answer;
+    let clothes_type = memory.twilio.collected_data.collect_clothes_order.answers.clothes_type.answer;
+    let num_clothes = memory.twilio.collected_data.collect_clothes_order.answers.num_clothes.answer;
+    let shipping_country = memory.twilio.collected_data.collect_clothes_order.answers.shipping_country.answer;
 
-    // console.log("First name: "+first_name);
-    // console.log("Clothes type: "+clothes_type);
-    // console.log("Num clothes: "+num_clothes);
+    console.log("First name: "+first_name);
+    console.log("Clothes type: "+clothes_type);
+    console.log("Num clothes: "+num_clothes);
+    console.log("country: "+shipping_country);
 
     // let message = "Ok "+first_name+". Your order for "+num_clothes+" "+clothes_type+" is now confirmed. Thank you for ordering with us";
     
