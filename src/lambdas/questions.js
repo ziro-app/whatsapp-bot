7650feecd73bff2313b46eac318cf30736138cb7
uillerroger <uillerrogermartins@gmail.com>
2020-02-11 11:35:33 -0300

    alteração lista

--- src/lambdas/questions.js
+++ src/lambdas/questions.js
@@ -8,21 +8,21 @@ const questions = async (event, context, callback) => {
                     "name": "leads_ziro",
                     "questions": [
                         {
-        		            "question": "*1)* Digite seu cnpj para sabermos se podemos atendê-lo",
+        		            "question": "1) Digite seu cnpj para sabermos se podemos atendê-lo",
                             "name": "cnpj_cliente",
                             "type": "Twilio.NUMBER_SEQUENCE"
         		        },
         		        {
-                            "question": "*2)* Vamos atendê-lo. Escolha um produto da lista abaixo para comprar\n\
-                                            vestido de festa           \n\
-                                            saia                       \n\
-                                            camisa                     \n\
-                                            t-shirt                    \n\
-                                            jeans                      \n\
-                                            legging                    \n\
-                                            shorts                     \n\
-                                            colete                     \n\
-                                            jaqueta                    \n\
+                            "question": "2) Vamos atendê-lo. Escolha um produto da lista abaixo para comprar\n\
+                                            vestido de festa\n\
+                                            saia\n\
+                                            camisa\n\
+                                            t-shirt\n\
+                                            jeans\n\
+                                            legging\n\
+                                            shorts\n\
+                                            colete\n\
+                                            jaqueta\n\
                                             Bikini",
                             "name": "lista_produtos1",
                             "type": "Lista"
