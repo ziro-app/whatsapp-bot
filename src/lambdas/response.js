c52211422734a659869e4b57fe3eed6b00fa1c74
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-10 20:35:15 -0800

    add try catch to improve error handling

--- src/lambdas/response.js
+++ src/lambdas/response.js
@@ -1,30 +1,42 @@
 const main = require('../templates/main')
 
 const response = async (event, context, callback) => {
-    const memory = JSON.parse(event.body.Memory)
-    console.log(memory)
-    const { first_name, clothes_type, num_clothes, shipping_country } = memory.twilio.collected_data.collect_clothes_order.answers
-    const nome = first_name.answer
-    const tipo = clothes_type.answer
-    const roupas = num_clothes.answer
-    const pais = shipping_country.answer
-    console.log(nome)
-    console.log(tipo)
-    console.log(roupas)
-    console.log(pais)
-    const message = "Ok " + nome + ". Sua compra de " + roupas + " " + tipo + " esta confirmada. Entregaremos no " + pais
-    const responseObject = {
-        "actions": [
-            {
-                "say": {
-                    "speech": message
-                } 
+    try {
+        if (event.body.Memory) {
+            const memory = JSON.parse(event.body.Memory)
+            console.log(memory)
+            const { first_name, clothes_type, num_clothes, shipping_country } = memory.twilio.collected_data.collect_clothes_order.answers
+            const nome = first_name.answer
+            const tipo = clothes_type.answer
+            const roupas = num_clothes.answer
+            const pais = shipping_country.answer
+            console.log(nome)
+            console.log(tipo)
+            console.log(roupas)
+            console.log(pais)
+            const message = "Ok " + nome + ". Sua compra de " + roupas + " " + tipo + " esta confirmada. Entregaremos no " + pais
+            const responseObject = {
+                "actions": [
+                    {
+                        "say": {
+                            "speech": message
+                        } 
+                    }
+                ]
             }
-        ]
-    }
-  	return {
-        statusCode: 200,
-        body: JSON.stringify(responseObject, null, 4)
+          	return {
+                statusCode: 200,
+                body: JSON.stringify(responseObject, null, 4)
+            }
+        } else {
+            return {
+                statusCode: 200,
+                body: 'Request feito no browser'
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        throw { statusCode: 500, body: error }
     }
 }
 
