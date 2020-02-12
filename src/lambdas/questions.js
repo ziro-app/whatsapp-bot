406fbb74df5034a65671f63f49b25cd75b745039
uillerroger <uillerrogermartins@gmail.com>
2020-02-12 16:47:41 -0300

    1 grupo

--- src/lambdas/questions.js
+++ src/lambdas/questions.js
@@ -16,71 +16,7 @@ const questions = async (event, context, callback) => {
                     }
                 }
             },
-            {
-                "collect": {
-                    "name": "leads_ziro",
-                    "questions": [
-                        {
-                            "question": "2) Escolha um produto da lista abaixo para comprar",
-                            "name": "lista_produtos1",
-                            "type": "Lista"
-                        },
-                        {
-                            "question": "3) Escolha mais um produto",
-                            "name": "lista_produtos2",
-                            "type": "Lista"
-                        },
-                        {
-                            "question": "4) Escolha o terceiro e último",
-                            "name": "lista_produtos3",
-                            "type": "Lista"
-                        }
-                    ],
-                    "on_complete": {
-                        "redirect": "https://whats.ziro.app/.netlify/functions/response1"
-                    }
-                }
-            },
-            {
-                "collect": {
-                    "name": "leads_ziro",
-                    "questions": [
-                        {
-                            "question": "5) A partir de que valor você vende um(a) ",
-                            "name": "venda_produto1",
-                            "type": "Twilio.NUMBER_SEQUENCE"
-                        },
-                        {
-                            "question": "6) A partir de que valor você vende um(a) ",
-                            "name": "venda_produto2",
-                            "type": "Twilio.NUMBER_SEQUENCE"
-                        },
-                        {
-                            "question": "7) A partir de que valor você vende um(a) ",
-                            "name": "venda_produto3",
-                            "type": "Twilio.NUMBER_SEQUENCE"
-                        }
-                    ],
-                    "on_complete": {
-                        "redirect": "https://whats.ziro.app/.netlify/functions/response2"
-                    }
-                }
-            },
-            {    
-                "collect": {
-                    "name": "leads_ziro",
-                    "questions": [    
-                        {
-                            "question": "Perfeito, qual é o melhor horário para entrarmos em contato? ",
-                            "name": "horario_contato",
-                            "type": "Twilio.TIME"
-                        }
-                    ],
-                    "on_complete": {
-                        "redirect": "https://whats.ziro.app/.netlify/functions/response3"
-                    }
-                }
-            }
+            
         ]
     }
     return {
