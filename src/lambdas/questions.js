f48a7e2e2e5cd749b01eebb0f476ce73d56aa113
uillerroger <uillerrogermartins@gmail.com>
2020-02-12 16:22:27 -0300

    reduzindo questions

--- src/lambdas/questions.js
+++ src/lambdas/questions.js
@@ -2,98 +2,83 @@ const main = require('../templates/main')
 
 const questions = async (event, context, callback) => {
     const responseObject = {
-        "actions": [
-            {
+        "actions": [{
                 "collect": {
                     "name": "leads_ziro",
-                    "questions": [
-                        {
-        		            "question": "1) Olá,                                                                                                                                                             Digite seu cnpj para sabermos se podemos atendê-lo",
-                            "name": "cnpj_cliente",
-                            "type": "Twilio.NUMBER_SEQUENCE"
-        		        },
-        		      
-	                ],
-    	            "on_complete": {
-    	                "redirect": "https://whats.ziro.app/.netlify/functions/response"
+                    "questions": [{
+                        "question": "1) Olá,                                                                                                                                                             Digite seu cnpj para sabermos se podemos atendê-lo",
+                        "name": "cnpj_cliente",
+                        "type": "Twilio.NUMBER_SEQUENCE"
+                    }],
+                    "on_complete": {
+                        "redirect": "https://whats.ziro.app/.netlify/functions/response"
                     }
                 }
             },
             {
                 "collect": {
-                    "name": "leads_ziro",
-                    "questions": [                      
-        		        {
-                            "question": "2) Escolha um produto da lista abaixo para comprar\
-                                            vestido de festa\
-                                            saia\
-                                            camisa\
-                                            t-shirt\
-                                            jeans\
-                                            legging\
-                                            shorts\
-                                            colete\
-                                            jaqueta\
-                                            Bikini",
+                    "name": "leads_ziro1",
+                    "questions": [{
+                            "question": "2) Escolha um produto da lista abaixo para comprar",
                             "name": "lista_produtos1",
                             "type": "Lista"
-        		        },
+                        },
                         {
-        		            "question": "3) Escolha mais um produto",
+                            "question": "3) Escolha mais um produto",
                             "name": "lista_produtos2",
                             "type": "Lista"
-        		        },
+                        },
                         {
-        		            "question": "4) Escolha o terceiro e último",
+                            "question": "4) Escolha o terceiro e último",
                             "name": "lista_produtos3",
                             "type": "Lista"
                         }
                     ],
-    	            "on_complete": {
-    	                "redirect": "https://whats.ziro.app/.netlify/functions/response1"
+                    "on_complete": {
+                        "redirect": "https://whats.ziro.app/.netlify/functions/response1"
                     }
                 }
             },
             {
                 "collect": {
-                    "name": "leads_ziro",
+                    "name": "leads_ziro2",
                     "questions": [
-                     
+    
                         {
-        		            "question": "5) A partir de que valor você vende um(a) " ,
+                            "question": "5) A partir de que valor você vende um(a) ",
                             "name": "venda_produto1",
                             "type": "Twilio.NUMBER_SEQUENCE"
-                        },                      
+                        },
                         {
-        		            "question": "6) A partir de que valor você vende um(a) " ,
+                            "question": "6) A partir de que valor você vende um(a) ",
                             "name": "venda_produto2",
                             "type": "Twilio.NUMBER_SEQUENCE"
                         },
                         {
-        		            "question": "7) A partir de que valor você vende um(a) " ,
+                            "question": "7) A partir de que valor você vende um(a) ",
                             "name": "venda_produto3",
                             "type": "Twilio.NUMBER_SEQUENCE"
-        	            }
-	                ],
-    	            "on_complete": {
-    	                "redirect": "https://whats.ziro.app/.netlify/functions/response2"
+                        }
+                    ],
+                    "on_complete": {
+                        "redirect": "https://whats.ziro.app/.netlify/functions/response2"
                     }
                 }
             },
             {
-                                
+    
                 "collect": {
-                    "name": "leads_ziro",
+                    "name": "leads_ziro3",
                     "questions": [
-                         
+    
                         {
-                            "question": "Perfeito, qual é o melhor horário para entrarmos em contato? " ,
+                            "question": "Perfeito, qual é o melhor horário para entrarmos em contato? ",
                             "name": "horario_contato",
                             "type": "Twilio.TIME"
                         }
                     ],
                     "on_complete": {
-                            "redirect": "https://whats.ziro.app/.netlify/functions/response3"
+                        "redirect": "https://whats.ziro.app/.netlify/functions/response3"
                     }
                 }
             }
