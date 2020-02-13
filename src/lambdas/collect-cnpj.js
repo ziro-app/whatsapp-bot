bd94a60e173f91f3694d4d17dd815311a9ea6f77
uillerroger <uillerrogermartins@gmail.com>
2020-02-12 21:21:46 -0300

    reduzindo lambdas

--- src/lambdas/collect-cnpj.js
+++ src/lambdas/collect-cnpj.js
@@ -26,3 +26,35 @@ const collectCnpj = async (event, context, callback) => {
 }
 
 exports.handler = main(collectCnpj)
+
+//AQUI ESTÃO O RESTANTE DAS PERGUNTAS
+
+// const responseObject = {
+//     "actions": [
+//         {
+//             "collect": {
+//                 "name": "leads_ziro",
+//                 "questions": [
+//                     {
+//                         "question": "5) A partir de quanto você vende um(a)"+ produto1,
+//                         "name": "venda_produto1",
+//                         "type": "Lista"
+//                     },
+//                     {
+//                         "question": "6) A partir de quanto você vende um(a)"+ produto2,
+//                         "name": "venda_produto2",
+//                         "type": "Lista"
+//                     },
+//                     {
+//                         "question": "7) A partir de quanto você vende um(a)"+ produto3,
+//                         "name": "venda_produto3",
+//                         "type": "Lista"
+//                     }
+//                 ],
+//                 "on_complete": {
+//                     "redirect": "https://whats.ziro.app/.netlify/functions/response"
+//                 }
+//             }
+//         }
+//     ]
+// }
