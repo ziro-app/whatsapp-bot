c5e9afb65764e0fdccc63e10c5e1e88f19608487
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-15 15:02:44 -0800

    refactoring

---
+++ src/model/dataTransformations.js
@@ -0,0 +1,34 @@
+const dataTransformations = (base, products, priceTable) => {
+	const transformedBase = base.map(supplier => {
+		const { Fabricante, Instagram, Preco, Bot, ...styles } = supplier 
+		return {
+			nome: Fabricante,
+			insta: Instagram,
+			estilos: Object.values(styles),
+			preco: Preco
+		}
+	})
+	const transformedProducts = products.map(product => {
+		const { fabricante, tipo, ...productList } = product
+		const productEntries = Object.entries(productList)
+		const productArrayList = productEntries.map(entry => {
+			const [productName, availability] = entry
+			if (availability === 's')
+				return productName
+			return null
+		}).filter(productName => !!productName)
+		return {
+			nome: fabricante,
+			produtos: productArrayList
+		}
+	})
+	const fullBase = transformedBase.map(supplier => {
+		const match = transformedProducts.find(product => product.nome === supplier.nome)
+		const { nome, produtos } = match
+		return { ...supplier, produtos }
+	})
+	console.log(priceTable)
+	return fullBase
+}
+
+module.exports = dataTransformations
--- src/model/index.js
+++ src/model/index.js
@@ -2,45 +2,19 @@ require('dotenv').config()
 const axios = require('axios')
 const config = require('./axiosConfig')
 const arrayObject = require('@ziro/array-object')
+const dataTransformations = require('./dataTransformations')
 const findSuppliers = require('./findSuppliers')
 
 const model = async () => {
 	try {
 		const { data: { valueRanges } } = await axios(config)
 		const [baseArray, productsArray, priceTableArray] = valueRanges
-		const base = arrayObject(baseArray)
-		const products = arrayObject(productsArray)
-		const priceTable = arrayObject(priceTableArray)
-		const transformedBase = base.map(supplier => {
-			const { Fabricante, Instagram, Preco, Bot, ...styles } = supplier 
-			return {
-				nome: Fabricante,
-				insta: Instagram,
-				estilos: Object.values(styles),
-				preco: Preco
-			}
-		})
-		const transformedProducts = products.map(product => {
-			const { fabricante, tipo, ...productList } = product
-			const productEntries = Object.entries(productList)
-			const productArrayList = productEntries.map(entry => {
-				const [productName, availability] = entry
-				if (availability === 's')
-					return productName
-				return null
-			}).filter(productName => !!productName)
-			return {
-				nome: fabricante,
-				produtos: productArrayList
-			}
-		})
-		const fullBase = transformedBase.map(supplier => {
-			const match = transformedProducts.find(product => product.nome === supplier.nome)
-			const { nome, produtos } = match
-			return { ...supplier, produtos }
-		})
-		console.log(fullBase)
-		const targetSuppliers = findSuppliers('calca', 'Médio', 'Casual', fullBase)
+		const base = dataTransformations(
+			arrayObject(baseArray),
+			arrayObject(productsArray),
+			arrayObject(priceTableArray)
+		)
+		const targetSuppliers = findSuppliers('calca', 'Médio', 'Casual', base)
 		console.log(targetSuppliers)
 		const targetNamesAndInstas = targetSuppliers.map(supplier => ({
 			nome: supplier.nome,
