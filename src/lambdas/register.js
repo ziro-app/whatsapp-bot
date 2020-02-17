69612c0fad7acf590ed97c78c6ed3e44a7f18b73
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-16 19:10:27 -0800

    fix

--- src/lambdas/register.js
+++ src/lambdas/register.js
@@ -36,12 +36,16 @@ const cnpjConfig = cnpj => ({
 const autopilot = async event => {
     try {
         if (event.body.Memory) {
-            console.log(event.body)
-            console.log(JSON.parse(event.body.Memory))
             if (!event.body.Memory.twilio.collected_data.register) return responseOk(register)
-            const cnpj = event.body.Memory.twilio.collected_data.register.answers.register.answer
-            const { data: { valueRanges } } = await axios(cnpjConfig(cnpj))
-            return responseOk(register)
+            const cnpj = event.body.CurrentInput
+            const cnpjIsValid = !!Number(cnpj) && cnpj.toString().length === 14
+            if (cnpjIsValid) {
+                const { data: { status, result } } = await axios(cnpjConfig(cnpj))
+                console.log('status',status)
+                console.log('result',result)
+                return responseOk(endRegister)    
+            }
+            return responseOk(endRegister)
         }
         throw createError(404, 'Invalid Twilio Request. Memory is empty')
     } catch (error) { throw error }
